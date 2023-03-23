5a4869c657b0379a4b10b75e184074d2f5dcc12d
xaunikmas <xaunikmas@gmail.com>
2023-03-23 22:43:34 +0700

    nambah

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 # multiple assignment, kalo value nya sama
-# niko = naunx = rino = 30
-# print (niko)
-# print (naunx)
-# print (rino)
+niko = naunx = rino = 30
+print (niko)
+print (naunx)
+print (rino)
 
 
-#  multiple assigment kalo beda value nya
-# niko, naunx, rino = 30, 31, 32
+# multiple assigment kalo beda value nya
+niko, naunx, rino = 30, 31, 32
 
 
 name = "niko noviyanto"
-# print (len(name))
-# print (name.find("k"))
-# print (name.upper())
-# print (name.lower())
-# print (name.isdigit())
-# print (name.isalpha())
-# print (name.count("i"))
-# print (name.replace("i", "a"))
-# print (name*4)
+print (len(name))
+print (name.find("k"))
+print (name.upper())
+print (name.lower())
+print (name.isdigit())
+print (name.isalpha())
+print (name.count("i"))
+print (name.replace("i", "a"))
+print (name*4)
---
+++ while-loop.py
@@ -0,0 +1,8 @@
+# while loop, pengulangan
+
+nama = ""
+
+while len(nama) == 0:
+    nama = input("enter your name ?   ")
+
+print("your name is " + nama)
