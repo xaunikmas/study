a70f1bffcf35e343abe460aa265846757bb29559
xaunikmas <xaunikmas@gmail.com>
2023-03-23 21:49:15 +0700

    nambah

---
+++ import_math.py
@@ -0,0 +1,22 @@
+import math
+
+pi = 4.001
+
+x, y, z = 30, 12, 72
+
+
+print (round(pi)) # round untuk membulatkan decimal
+
+print (math.ceil(pi))  # math.ceil untuk pembulatan ke atas, dari 3.001 ke 4
+
+print (math.floor(pi)) # math.floor untuk pembulatan ke bawah, dari 3.001 ke 3
+
+print (abs(pi))   #abs utk nilai yang absolute. dari minus ke plus
+
+print (pow(pi, 2)) # pow untuk mengkuadratkan angka
+
+print (math.sqrt(420)) #sqrt untuk menjumlah square root, biasanya utk penjumlahan segitiga sama kaki
+
+print (max(x, y, z)) # fungsi max untuk mencari angka/nilai terbesar dari sebuah atau deretan variable
+
+print (min(x, y, z)) # fungsi min untuk mencari angka/nilai terkecil dari sebuah atau deretan variable
