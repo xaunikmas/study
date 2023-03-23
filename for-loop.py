f054a875fb9c1e425c0db19e9f80847f041c4ed4
xaunikmas <xaunikmas@gmail.com>
2023-03-23 23:04:28 +0700

    nambah

---
+++ for-loop.py
@@ -0,0 +1,14 @@
+# for loop = statement that will execute it's block of code
+#            in a limited amount of time
+# while loop = unlimited or infinite loop
+# for loop   = limited
+
+# for j in range (10):
+#     print (j + 1)
+
+import time # untuk import feat waktu
+
+for j in range (10, 0, -1):
+    print (j)
+    time.sleep(1)
+print("THANK GOD IT's FRIDAY !!")
--- while-loop.py
+++ while-loop.py
@@ -2,7 +2,16 @@
 
 nama = ""
 
-while len(nama) == 0:
-    nama = input("enter your name ?   ")
+while len(nama) == 0: # kondisi dimana input nama nya kosong, alias sama dengan null, maka akan loop terus meminta input argument nya
+    nama = input("enter your name ?   ")  # result :  enter your name ?  
 
-print("your name is " + nama)
+print("your name is " + nama)  # result : your name is niko
+
+# atau pake metode lain, speerti dibawah :
+
+nama = None
+
+while not nama:
+    nama = input ("please, enter your name?  ")
+
+print ("your name is " + nama)
