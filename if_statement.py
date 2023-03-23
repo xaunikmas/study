4373745b930783f148945a66c20aa2188e4a1363
xaunikmas <xaunikmas@gmail.com>
2023-03-23 22:18:40 +0700

    nambah

---
+++ if_statement.py
@@ -0,0 +1,10 @@
+age = int(input("enter your age "))
+
+if age < 40:
+    print ("you are an adult")
+elif age > 45:
+    print ("you are an older")
+elif age < 18:
+    print("you are a teenager")
+else:
+    print ("you are a child")
