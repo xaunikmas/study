ad99ca51399ce170071e10d2dcc76e89ff89bed6
xaunikmas <xaunikmas@gmail.com>
2023-03-27 22:42:02 +0700

    nambah

---
+++ break-continue-pass.py
@@ -0,0 +1,11 @@
+#  Loop control statement = change a loop execution from its normal sequence
+
+#  break    = used to terminate the loop seluruhnya
+#  continue = skip to the next iteration of the loop
+#  pass     = does nothing, acts as a placeholder
+
+
+while True:
+    name = input ("input your name, please? ")
+    if name != "":
+        break
