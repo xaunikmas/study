54d6f0f45441782bade97a4d0ed056d3b5c1d2ca
xaunikmas <xaunikmas@gmail.com>
2023-04-01 22:32:27 +0700

    nambah

---
+++ assignment.py
@@ -0,0 +1,18 @@
+# OPERASI YANG DAPAT DILAKUKAN DENGAN PENYINGKATAN 
+# OPERASI DENGAN ASSIGNMENT
+
+a = 5
+
+a += 1  # a = a + 1
+
+a -= 1  # a = a - 1
+
+a *= 2  # a = a * 2
+ 
+a -= 2  # a = a - 2
+
+a/= 2  # a / 2
+
+a%= 3  # a = a % 3
+
+print (a)
---
+++ string.py
@@ -0,0 +1,9 @@
+print ("""
+Nama : niko
+kelas : 2
+job : software developer
+""")
+# multiline literal string, bisa bikin value/argument yang banyak
+
+print (r"c:\windows\user\application_data")  # r = raw string. dibelakang R smeua dianggap string
+
