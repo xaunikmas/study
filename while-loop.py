d26427efe6f01f4cf5c6dbbe2084b90845baa6c3
xaunikmas <xaunikmas@gmail.com>
2023-04-02 16:04:09 +0700

    nambah

--- while-loop.py
+++ while-loop.py
@@ -2,7 +2,7 @@
 
 nama = ""
 
-while len(nama) == 0: # kondisi dimana input nama nya kosong, alias sama dengan null, maka akan loop terus meminta input argument nya
+while len(nama) == 0:  # kondisi dimana input nama nya kosong, alias sama dengan null, maka akan loop terus meminta input argument nya
     nama = input("enter your name ?   ")  # result :  enter your name ?  
 
 print("your name is " + nama)  # result : your name is niko
@@ -15,3 +15,28 @@ while not nama:
     nama = input ("please, enter your name?  ")
 
 print ("your name is " + nama)
+
+
+import time
+
+hitung = 1
+while (hitung < 4):
+    print ('...', hitung)
+    time.sleep (1)
+    hitung = hitung + 1
+
+nama ='Adieu nixau'
+print (nama.upper())
+
+# PENGGUNAAN FOR LOOP  #
+
+angka = [1,2,3,4,5]
+for x in angka:
+    print (x)
+
+buah = ['nanas', 'jambu', 'melon', 'pepaya']
+
+for isi in buah:
+    print ('saya suka makan buah',isi, '\n')
+
+print ('saya paling suka dengan buah', buah[3], '\n')
