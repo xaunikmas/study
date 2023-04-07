1068aa24eb5bd43622f6006491ce7fdb7eb0c3e3
xaunikmas <xaunikmas@gmail.com>
2023-04-07 18:40:31 +0700

    nambah

--- OOP.py
+++ OOP.py
@@ -1,35 +1,40 @@
-# class Karyawan:
-#     def __init__(self, inputName, inputJob, inputAdd, inputCity):
-#         self.name = inputName
-#         self.job = inputJob
-#         self.address = inputAdd
-#         self.city = inputCity
-        
-# karyawan1 = Karyawan ('niko','software dev', 'rawamangun', 'Jakarta')
-# karyawan2 = Karyawan ('anunx', 'network engineer', 'bintaro', 'tangsel')
-# karyawan3 = Karyawan ('rino', 'marketing agent', 'cempaka putih', 'Jakarta')
-
-# print (karyawan1.__dict__)
-# print (karyawan2.__dict__)
-# print (karyawan3.__dict__)
-
-##### CONSTRUCTOR ######
-
 class Item:
-    def __init__(self, name, type, price, quantity):
-        
+    pay_rate = 0.8  # INI ADALAH DISCOUNT 20%
+    def __init__(self, name: str, brand: str, price: float, quantity):
+    
+        # RUN VALIDATIONS TO THE RECEIVED ARGUMENT
+        assert price >= 0, f'price {price} must greater than 0'
+        assert quantity >= 0, f'Quantity {quantity} must greater than 0'
+
+
+        # ASSIGN TO SELF OBJECT
         self.name = name
-        self.type = type
+        self.brand = brand
         self.price = price
         self.quantity = quantity
 
-    def totalHarga(self):
-        return self.price * self.quantity
-    
-item1 = Item('phone', 'smartphone', 1000, 10)
-print (f'total harga dan jumlah smartphone:', item1.totalHarga())
+    def diskon (self):
+        self.price = self.price * Item.pay_rate    
         
-item2 = Item('laptop', 'macbook', 2000, 10)
-print (f'total harga dan jumlah laptop macbook:', item2.totalHarga())
+item1 = Item ('laptop', 'HP', 1000, 5)
+item1.diskon()
+
+item2 = Item ('chromebook', 'Dell', 100, 5)
+item2.diskon()
+
+item3 = Item ('smartphone', 'Apple', 1000, 10)
+item3.diskon()
+
+item4 = Item ('smartphone', 'Samsung', 500, 100)
+item4.diskon()
+
+item5 = Item ('smartphone', 'Redmi', 200, 100)
+item5.diskon()
+
+item6 = Item ('smartphone', 'Oppo', 300, 200)
+item6.diskon()
+
+item7 = Item ('smartphone', 'Xiaomi', 200, 100)
+item7.diskon()
 
 print (item1.__dict__)
