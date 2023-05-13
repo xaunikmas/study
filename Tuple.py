bd70655ee06f709eb173f6215a0a10a38c0a53b2
xaunikmas <xaunikmas@gmail.com>
2023-05-13 18:21:01 +0700

    nambah

---
+++ Tuple.py
@@ -0,0 +1,8 @@
+# tupple beda dengan lists. karena tupple menggunakan () pada variable, sedangkan lists menggunakan []
+
+nomers = (1, 'home', 2, False, 4, 5)
+setring = ('home', 'earth', 'world')
+
+print (nomers)
+
+#Tuple the best option kalo bekerja untuk angka, koordinat yang permanent, alias ga akan berubah. kalo list bisa berubah
--- list.py
+++ list.py
@@ -1,11 +1,18 @@
 #  LIST INDEX
 #  list index = used to store multiple items in a single variable
 
-makanan = ["pizza", "burger", "ketoprak"]
-makanan [1] = "satay"
+# makanan = ["pizza", "burger", "ketoprak"]
 
 # makanan.append("gado-gado")
-# makanan.pop()
 
-for i in makanan:
-    print (i)
+# for i in makanan:
+#     print (i,'\n')
+
+
+grup1 = [1,2,3,4,5]
+grup2 = ['banana', 'apples', 'mangos', 'orange', 'jambu', 'nanas', 'durian']
+
+print (grup2)
+
+
+
