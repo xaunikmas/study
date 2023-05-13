2053775fba28c710bc107d43fda3c0aa967f1b68
xaunikmas <xaunikmas@gmail.com>
2023-05-13 22:23:21 +0700

    nambah

--- Function.py
+++ Function.py
@@ -1,27 +1,38 @@
-def hay (nama, umur):
-    print ('welcome ' + nama, 'you are', umur, 'years old', '\n')
+# def hay (nama, umur):
+#     print ('welcome ' + nama, 'you are', umur, 'years old', '\n')
 
-#penggunaan function begini bisa, atau
-hay ('niko', 40)
+# #penggunaan function begini bisa, atau
+# hay ('niko', 40)
 
-#begini
-hay (nama = 'niko', umur = 40)
-
-
-# penggunaan asterix jika kita ingin isi lebih dari 1 argument, dan bisa di spesifikan dengan indexing nya
-def halo (*names):
-    print ('welcome back ', names, '\n')
-
-halo ('niko', 'naunx', 'rino')
+# #begini
+# hay (nama = 'niko', umur = 40)
 
 
-def sapa (nama, umur, alamat):
-    print ('welcome, ' + 'your name is ' + str(nama) + ', your age is ' + str(umur) + ' & lived in ' + str(alamat))
+# # penggunaan asterix jika kita ingin isi lebih dari 1 argument, dan bisa di spesifikan dengan indexing nya
+# def halo (*names):
+#     print ('welcome back ', names, '\n')
 
-nama = input ('please enter your name? ')
-umur = input ('enter your age ')
-alamat = input ('enter your address ')
+# halo ('niko', 'naunx', 'rino')
 
-sapa (nama, umur, alamat)
 
+# def sapa (nama, umur, alamat):
+#     print ('welcome, ' + 'your name is ' + str(nama) + ', your age is ' + str(umur) + ' & lived in ' + str(alamat))
+
+# nama = input ('please enter your name? ')
+# umur = input ('enter your age ')
+# alamat = input ('enter your address ')
+
+# sapa (nama, umur, alamat)
+
+
+
+# RETURN FUNCTION
+def kali (num1, num2):
+    print ('your result is = ')
+    return num1 * num2
+
+num1 = int(input('masukkan nomer yang anda pilih '))
+num2 = int(input('masukkan nomer yang anda pilih '))
+
+print (kali(num1, num2))
 
--- if_statement.py
+++ if_statement.py
@@ -1,12 +1,22 @@
-age = int(input("enter your age "))
+# kalimat = input ('masukkan sebuah kalimat ')
 
-if age == 40:
-    print ("you are an adult")
-elif age > 45:
-    print ("you are an older")
-elif age > 11:
-    print("you are a teenager")
-elif age <= 10 :
-    print ("you are a child")
+# nama1 = input ('masukkan kalimat yang diganti ')
+# nama2 = input ('masukkan kalimat pengganti ')
+# print (kalimat.replace(nama1, nama2))
+ 
+
+# IF STATEMENT USE
+
+nilai = int(input('masukkan score '))
+
+if nilai <= 40:
+    print ('you need to learn more ')
+elif nilai > 41 and nilai < 78:
+    print ('you need to spend time to learn ')
+elif nilai >= 79 and nilai <= 90:
+    print ('well done boy ')
+elif nilai >= 90:
+    print ('you are smart ass')
 else:
-    print ("you are a cute baby")
+    ('you are dumb ')
+
